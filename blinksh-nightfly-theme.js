67f482485acb18638722875b60fe0c6eec0c6f44
NaCIO-Ghrelin <63216246+NaCIO-Ghrelin@users.noreply.github.com>
2021-12-02 13:02:55 +1000

    Create blinksh-nightfly-theme.js

---
+++ blinksh-nightfly-theme.js
@@ -0,0 +1,23 @@
+t.prefs_.set('color-palette-overrides',
+	[
+		"#1d3b53",
+		"#fc514e",
+		"#a1cd5e",
+		"#e3d18a",
+		"#82aaff",
+		"#c792ea",
+		"#7fdbca",
+		"#a1aab8",
+		"#7c8f8f",
+		"#ff5874",
+		"#21c7a8",
+		"#ecc48d",
+		"#82aaff",
+		"#ae81ff",
+		"#7fdbca",
+		"#d6deeb"
+	]
+);
+t.prefs_.set('foreground-color', "#acb4c2");
+t.prefs_.set('background-color', "#011627");
+t.prefs_.set('cursor-color',     'rgba(156,161,170,0.5)');
